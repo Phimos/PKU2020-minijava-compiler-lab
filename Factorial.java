2b3e70a516b5aa0e425de8fcbd2f87b27b6ffee6
Phimos <41637559+Phimos@users.noreply.github.com>
2020-03-22 22:49:00 +0800

    lab1 final

--- Factorial.java
+++ Factorial.java
@@ -1,43 +1,44 @@
-class Test{
-    public static void main(String[]a){
-        System.out.println(new Start().start());
-    }
+class Test8{
+	public static void main(String[]a){
+		
+	}
 }
 
-class B extends Start{
-    Start ab;
-}
+class A{
 
-class C extends B{
-    public int start(boolean sdf){
-        return 0;
-    }
 }
+class B extends A{
 
+}
 
 class Start{
-    Start star;
-    int j;
-    int i;
+	boolean num1;
+	public B sstart(int num2, A b1){
+		int[] ll;
+		int t;
+		A a;
+		B b;
+		a = new A();
+		b = new B();
+		a = new B();
+		//b = new A();
+		ll = new int[this.start()];
+		num1 = false;
+		t = ll.length;
+		return b;
+	}
 
-    public int plusone(int w,int ww){
-        w = w + 1;
-        return w;
-    }
+	public int start(){
+		int ii;
+		B b;
+		ii = 1;
+		b = new B();
+		b = this.sstart(ii, b);
+		b = this.sstart(ii, this.sstart(ii, b));
+		return 0;
+	}
+	// public int start(int i){
+	// 	return 0;//重载
+	// }
 
-    public int start(boolean asdf){
-        star = this.next();
-        return 0;
-    }
-
-    public int faslkdjf(int asdf){
-        asdf = this.plusone(this.plusone(this.plusone(asdf, asdf), asdf), this.plusone(asdf, asdf));
-        return asdf;
-    }
-
-    public Start next(){
-        B bb;
-        bb = new B();
-        return bb;
-    }
 }
