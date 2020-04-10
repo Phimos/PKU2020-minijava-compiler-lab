c226dfa74221cc35ebe5c72dcea67d5cf540879c
张灏宇 <zhanghaoyu@zhanghaoyudeMacBook-Pro.local>
2020-04-10 12:30:09 +0800

    add to MType

--- symbol/MType.java
+++ symbol/MType.java
@@ -28,4 +28,51 @@ public class MType{
     public void setName(String _name){
         this.name = _name;
     }
+
+    // ---for piglet---
+
+    //null represent it hasn't been initialized
+    protected String pigName = null;
+    public void setPigName(String pigName){
+        this.pigName = pigName;
+    }
+    public String getPigName(){
+        return this.pigName;
+    }
+
+
+    // preprocessing 
+
+    protected int pigStatus = -1;
+    // -1 : not initalized
+    // 0 : already completed classComplete()
+    // 1 : done allocTemp()
+    public void setPigStatus(int pigSta){
+        this.pigStatus = pigSta;
+    }
+    public int getPigStatus(){
+        return this.pigStatus;
+    }
+
+    protected int offset = -1; //the offset in class
+    public void setOffset(int off) {
+        this.offset = off;
+    }
+    public int getOffset() {
+        return this.offset;
+    }
+
+    protected int Temp = 0;
+    public void setTempNum(int tempNum) {
+        this.Temp = tempNum;
+    }
+
+    public int getTempNum() {
+        return this.Temp;
+    }
+
+    public boolean pdTemp(){
+        return this.Temp > 0; // -1 represent not initialized
+    }
+
 }
