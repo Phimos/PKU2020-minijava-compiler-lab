7a5de2f93fd9b42d6004e33535f31146a7b48a8f
Phimos <41637559+Phimos@users.noreply.github.com>
2020-04-21 16:14:11 +0800

    lab3 finished

--- lab3test.sh
+++ lab3test.sh
@@ -7,6 +7,10 @@ do
         echo $file
         java Main $file
         java -jar spp.jar < ${file%.*}_my.spg
+        java -jar pgi.jar < ${file%.*}_my.spg > ${file%.*}_my.txt
+        java -jar pgi.jar < ${file%.*}.spg > ${file%.*}.txt
+        echo "Difference:"
+        diff ${file%.*}.txt ${file%.*}_my.txt
     fi
 done
-
+rm ./test/lab3/*.txt
